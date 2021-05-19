77a583ce5f4bd9871e7fd6db56fd3d80c89088e8
KImSuim <suim7171@naver.com>
2021-05-19 16:29:07 +0900

    로그인 뇌 생성

---
+++ js/login.js
@@ -0,0 +1,17 @@
+window.onload=function signIn() {
+    id=document.getElementById("id");
+    console.log(id.value);
+    password = document.getElementById("password");
+    console.log(password.value);
+    if(id.value =="id") {
+        if(password.value == "suim") {
+            window.location.href = "http://127.0.0.1:5501/profile.html";
+        }
+        else {
+            alert("비밀번호가 틀렸습니다.")
+        }
+    }
+    else {
+        alert("아이디 또는 비밀번호가 틀렸습니다.")
+    }
+}
