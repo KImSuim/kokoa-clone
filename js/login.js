fb484b6820b7c58129c8ac47612698bbae128afc
KImSuim <suim7171@naver.com>
2021-09-07 13:15:27 +0900

    a

--- js/login.js
+++ js/login.js
@@ -1,17 +1,25 @@
 window.onload=function signIn() {
-    id=document.getElementById("id");
-    console.log(id.value);
-    password = document.getElementById("password");
-    console.log(password.value);
-    if(id.value =="id") {
-        if(password.value == "suim") {
-            window.location.href = "profile2.html";
+    id= document.getElementById("id");
+    console.log(id.placeholder)
+    password=document.getElementById("password");
+    console.log(password.placeholder)
+    Loggingin = document.getElementById("Log-in");
+    
+    function Login() {
+        id=document.getElementById("id");
+        console.log(id.value);
+        password = document.getElementById("password");
+        console.log(password.value);
+        if(id.value =="Kim") {
+            if(password.value == "suim") {
+                window.location.href = "https://www.naver.com/";
+            }
+            else {
+                alert("비밀번호가 틀렸습니다.")
+            }
         }
         else {
-            alert("비밀번호가 틀렸습니다.")
+            alert("아이디 또는 비밀번호가 틀렸습니다.")
         }
-    }
-    else {
-        alert("아이디 또는 비밀번호가 틀렸습니다.")
     }
 }
